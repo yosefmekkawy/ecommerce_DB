2961f80e2df8a2c7f7b509d1e5fe4784bf649fb0
Yosef Mekkawy <115350570+yosefmekkawy@users.noreply.github.com>
2024-08-25 18:31:18 +0300

    fixed issues

--- Models/Cart.php
+++ Models/Cart.php
@@ -12,7 +12,6 @@ class Cart
 
     public function addProduct($user_id, $product_id, $quantity = 1)
     {
-        // Check if the product is already in the cart
         $query = "SELECT quantity FROM " . $this->table_name . " WHERE user_id = :user_id AND product_id = :product_id";
         $stmt = $this->conn->prepare($query);
         $stmt->bindParam(':user_id', $user_id);
